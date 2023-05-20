c985044b552251e47d46c63da1c28b04f2e4273a
Itserge1 <realitserge@gmail.com>
2023-05-20 14:55:25 -0400

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,27 @@
-import logo from './logo.svg';
 import './App.css';
+import {useEffect, useState} from 'react';
+import axios from 'axios';
 
 function App() {
+  const [image, setImage] = useState({})
+  
+  useEffect(() => {
+    axios.get("http://localhost:8000/api")
+      .then(res => {
+        console.log(res);
+        setImage(res.data)
+      })
+      .catch(err => {
+        console.log({errso: err});
+      })
+  }, [])
+  
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Fox name: {image.name} </h1>
+      <div className="imageq">
+        <img src={image.img} alt="Foximage"></img>
+      </div>
     </div>
   );
 }
